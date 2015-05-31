2e5c4c4fdcd064d13c0adcb57b46eeb36b16e561
Noahb930 <noahb930@gmail.com>
2015-05-30 20:36:31 -0400

    changed web.rb

--- web.rb
+++ web.rb
@@ -11,7 +11,17 @@ if ENV['RACK_ENV'] == 'development'
 else
   DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 end
-
+Class Flavor
+include DataMapper::Resource
+property :id, Serial
+property :name, String
+property :cost_in_cents, Integer
+end
+DataMapper.finalize
+Flavor.auto_upgrade!
+get '/flavors/new' do
+erb:'flavor/new'
+end
 get '/' do
 "<h1>WELCOME TO NOAH'S ICECREAM PARLOR!</h1>"
 end
