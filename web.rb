0bfd8c132f7c2b5ff5bd5059bc044b2c68d3d6b3
Noahb930 <noahb930@gmail.com>
2015-05-31 19:41:15 -0400

    changed web.rb

--- web.rb
+++ web.rb
@@ -18,10 +18,17 @@ class Flavor
   property :name, String
   property :cost, Float
 end
+class Customer
+  include DataMapper::Resource
+  property :id, Serial
+  property :fname, String
+  property :lname, String
+end
 DataMapper.finalize
 Flavor.auto_upgrade!
+Customer.auto_upgrade!
 get '/flavors/new' do
-erb :'flavor/new'
+erb :'flavors/new'
 end
 post '/flavors' do
 @flavor = Flavor.create(params[:flavor])
@@ -29,7 +36,21 @@ redirect to("/flavors/#{@flavor.id}")
 end
 get '/flavors/:id' do
 if @flavor = Flavor.first( id: params[:id])
-erb :'flavor/show' , locals: { flavor: @flavor}
+erb :'flavors/show' , locals: { flavor: @flavor}
+
+end
+end
+#_______________________________
+get '/customers/new' do
+erb :'customers/new'
+end
+post '/customers' do
+@customer = Customer.create(params[:customer])
+redirect to("/customers/#{@customer.id}")
+end
+get '/customers/:id' do
+if @customer = Customer.first( id: params[:id])
+erb :'customers/show' , locals: { customer: @customer}
 
 end
 end
